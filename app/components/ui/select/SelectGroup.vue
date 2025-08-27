5962700907f74b37b8af3203593d1ae3f67c4638
Nayan Thakor <nayan.thakor@aimsinfosoft.com>
2025-08-27 14:12:19 +0530

    resolve conflict which are occures due to previos merge

---
+++ app/components/ui/select/SelectGroup.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import type { SelectGroupProps, SelectGroup } from "reka-ui";
+
+const props = defineProps<SelectGroupProps>();
+</script>
+
+<template>
+  <SelectGroup data-slot="select-group" v-bind="props">
+    <slot />
+  </SelectGroup>
+</template>
--- app/schemas/auth.ts
+++ app/schemas/auth.ts
@@ -5,4 +5,15 @@ export const loginSchema = z.object({
   password: z.string(),
 });
 
+
+export const registrationSchema = z.object({
+  type: z.string().nonempty("Account type is required"),
+  locale: z.string().nonempty("Locale is required"),
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(8, "Please make a Strong Password"),
+});
 export type LoginSchema = z.infer<typeof loginSchema>;
+export type RegisterInput = z.infer<typeof registrationSchema>;
+
+
+
