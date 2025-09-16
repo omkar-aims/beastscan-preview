91b623ea7b7f6457695638c4eb02316ed5eff00c
Ajay Prajapati <sales.aimsinfosoft@gmail.com>
2025-09-16 18:05:21 +0530

    Deployement bug fix

--- app/composables/auth/useRegister.ts
+++ app/composables/auth/useRegister.ts
@@ -1,22 +1,21 @@
 import { ref } from "vue";
-import apiRoutes from "~/constants/apiRoutes";
 import type { RegisterSchema } from "~/schemas/auth";
 import type { RegisterResponse } from "~/types/auth";
 
-export const useRegister   = () => {
-  const routes = apiRoutes();
+export const useRegister = () => {
+  const apiRoutes = useApiRoutes();
 
   const pending = ref(false);
   const error = ref<string | null>(null);
 
   const register = async (
     credentials: RegisterSchema
-  ):Promise<RegisterResponse | undefined> => {
+  ): Promise<RegisterResponse | undefined> => {
     pending.value = true;
     error.value = null;
 
     try {
-      const res: RegisterResponse= await $fetch(routes.auth.register, {
+      const res: RegisterResponse = await $fetch(apiRoutes.auth.register, {
         method: "POST",
         body: credentials,
       });
