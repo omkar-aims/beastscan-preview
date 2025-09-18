00802eec00f6981cabd713476561691eaa092805
Ajay Prajapati <sales.aimsinfosoft@gmail.com>
2025-09-18 17:52:33 +0530

    Fixed cors issue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,14 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
+  // Enable CORS
+
+  nitro: {
+    routeRules: {
+      "/api/**": { cors: true },
+    },
+  },
+
   runtimeConfig: {
     public: {
       apiBase: process.env.NUXT_PUBLIC_API_BASE,
